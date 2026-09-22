fa2a254be307ebf8cd433977fa278881c1132ae0
ryugamons <robiana.nana@gmail.com>
2026-09-22 12:25:02 +0800

    version 1.09

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 26
         targetSdk = 37
         versionCode = 1
-        versionName = "1.08"
+        versionName = "1.09"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
